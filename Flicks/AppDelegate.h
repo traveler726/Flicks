8082b63939c302f66ef18a0bc3742eead39c344b
Jennifer Beck <jjbeck@yahoo-inc.com>
2017-01-23 09:14:59 -0800

    Initial Commit

---
+++ Flicks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flicks
+//
+//  Created by Jennifer Beck on 1/23/17.
+//  Copyright © 2017 JenniferBeck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Flicks/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Flicks
+//
+//  Created by Jennifer Beck on 1/23/17.
+//  Copyright © 2017 JenniferBeck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
